9b11e7bfc6a15d483eefbc38b132813b85081a97
nabajepo <nabajepo@gmail.com>
2024-11-15 08:36:13 -0500

    Update VoraciousAlgorithms.ml

--- VoraciousAlgorithms.ml
+++ VoraciousAlgorithms.ml
@@ -16,10 +16,40 @@ and edge = {
     vertexOne: vertex option ref;
     vertexTwo: vertex option ref;
 }
+type graph ={
+    vertices:vertex list;
+    edges:edge list
+}
 (*----------step2------------*)
 (*duplicate_vertex*)
-(*----------step3------------*)
-(*isEnoughVertex*)(*we must have at least two vertex*)
-(*isEnoughEdge*)(*we must have at least n-1 edges*)
-(*----------step4------------*)
+let rec duplicate_vertex lst eltIndex index rep=(*here we return 0 if there is not duplicate for a 
+                                                   specific element in a list and 1 if there is a duplicate*)
+    if (index=List.length lst)  then rep
+    else
+        let element=List.nth lst eltIndex in
+        let elementCompare=List.nth lst index in
+        if ((element<>elementCompare) &&
+            (eltIndex<>index)) then duplicate_vertex lst eltIndex (index+1) 0
+        else
+            1
+let rec duplicate_vertices lst index rep=(*check if a list has duplicate vertices *)
+    if(index=List.length lst) then rep
+    else
+        let resp=duplicate_vertex lst index 0 0 in
+        if(resp=0) then duplicate_vertices lst (index+1) resp 
+        else
+            1        
+
+(*----------step3------------*)(*Here we check if a graph is connected *)
+(*connect_graph*)(*here me make graphe after connecter each graph with his adjencies*)    
+(*getVerticesList*)(*only a list before making types and we must ask how many vertex we want from 2 to infinite*)
+(*getEdges*)(*we must have at most n-1 and we have to presente it  *)
+(*----------step4-----------*)(*we make vorociousAlgorithmes*)
+(*kruskal*)
+(*Prim *)
+(*Dijkstra*)
+(*----------step5-----------*)
+(*explanation*)(*here we get a location and create a txt which explain our procedures*)
+(*----------step6-----------*)
+(*voraciousAlgorithms*)
 (*-----------------------------------------END-------------------------------------------*)
